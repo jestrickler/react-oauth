dfc95a2701aed35c8e57cad2280e87a07d39e88c
John Strickler <strickler.7@osu.edu>
2019-11-25 10:09:28 -0500

    OAuth2 using AppAuth library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
-import {AuthorizationRequest, AuthorizationServiceConfiguration, RedirectRequestHandler} from "@openid/appauth";
+import {
+  AuthorizationRequest,
+  AuthorizationServiceConfiguration,
+  BaseTokenRequestHandler, FetchRequestor,
+  GRANT_TYPE_AUTHORIZATION_CODE,
+  GRANT_TYPE_REFRESH_TOKEN,
+  RedirectRequestHandler,
+  TokenRequest
+} from "@openid/appauth";
 
 export default function App() {
 
-  let authUri = 'https://accounts.google.com/o/oauth2/v2/auth';
-  let tokenUri = 'https://oauth2.googleapis.com/token';
-  let clientId = '242389526169-go0ks1io09t3eqk5j9u9nfo922e1q79o.apps.googleusercontent.com';
-  let redirectUri = 'http://localhost:3000/callback';
-  let scope = 'profile';
+  const authUri = 'https://accounts.google.com/o/oauth2/v2/auth';
+  const tokenUri = 'https://oauth2.googleapis.com/token';
+  const clientId = '242389526169-go0ks1io09t3eqk5j9u9nfo922e1q79o.apps.googleusercontent.com';
+  const redirectUri = 'http://localhost:3000/callback';
+  const scope = 'profile';
 
-  let configuration = new AuthorizationServiceConfiguration({
+  const configuration = new AuthorizationServiceConfiguration({
     authorization_endpoint: authUri,
     token_endpoint: tokenUri
   });
-  let authorizationHandler = new RedirectRequestHandler();
+  const authorizationHandler = new RedirectRequestHandler();
+  const tokenHandler = new BaseTokenRequestHandler(new FetchRequestor());
 
-  // create a request
-  let request = new AuthorizationRequest({
+  const authRequest = new AuthorizationRequest({
     client_id: clientId,
     redirect_uri: redirectUri,
     scope: scope,
@@ -26,12 +34,15 @@ export default function App() {
     extras: {'prompt': 'consent', 'access_type': 'offline'}
   });
 
+  let tokenResponse = null;
+
+
   const Routes = () => {
     return (
       <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/callback" component={Callback} />
-        <Route path="/" component={Home} />
+        <Route path="/login" component={Login}/>
+        <Route path="/callback" component={Callback}/>
+        <Route path="/" component={Home}/>
       </Switch>
     )
   }
@@ -41,23 +52,54 @@ export default function App() {
     return <h2>Home</h2>;
   }
 
+  const Login = props => {
+    console.log("Login", props);
+    authorizationHandler.performAuthorizationRequest(configuration, authRequest);
+    return null;
+  }
+
   const Callback = props => {
     console.log('Callback', props);
     const params = new URLSearchParams(props.location.search);
+    const code = params.get('code');
+
+    useEffect(() => {
+      console.log('performTokenRequest', code);
+
+      const tokenRequest = new TokenRequest({
+        client_id: clientId,
+        redirect_uri: redirectUri,
+        grant_type: GRANT_TYPE_AUTHORIZATION_CODE,
+        code: code,
+        refresh_token: undefined,
+        extras: {}
+      });
+      //
+      // const refreshRequest = new TokenRequest({
+      //   client_id: clientId,
+      //   redirect_uri: redirectUri,
+      //   grant_type: GRANT_TYPE_REFRESH_TOKEN,
+      //   code: undefined,
+      //   refresh_token: tokenResponse.refreshToken,
+      //   extras: undefined
+      // });
+
+      // returns an error from google about missing client_secret, but at least it's making the call
+      tokenHandler.performTokenRequest(configuration, tokenRequest)
+        .then(response => {
+          tokenResponse = response;
+          console.log('TokenResponse', tokenResponse);
+        }, []);
+    });
+
     return (
       <React.Fragment>
         <h2>Callback</h2>
-        <h4>Code: {params.get('code')}</h4>
+        <h4>Code: {code}</h4>
         <h4>State: {params.get('state')}</h4>
         <h4>Scope: {params.get('scope')}</h4>
       </React.Fragment>
     );
-  }
-
-  const Login = props => {
-    console.log("Login", props);
-    authorizationHandler.performAuthorizationRequest(configuration, request);
-    return null;
   }
 
   return (
@@ -76,5 +118,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
-
-
